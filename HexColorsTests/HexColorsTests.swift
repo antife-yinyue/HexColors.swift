47e7dae3607cb00da222d4a65cd8d0da6761e44e
wǒ_is神仙 <i@mrzhang.me>
2016-03-15 01:18:14 +0800

    Add test case

--- HexColorsTests/HexColorsTests.swift
+++ HexColorsTests/HexColorsTests.swift
@@ -1,28 +1,26 @@
+import UIKit
 import XCTest
 @testable import HexColors
 
 class HexColorsTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testThreeCharacterCount() {
+        let color = UIColor(hex: "#FFF")
+        XCTAssertNotNil(color, "Should not be nil.")
+    }
+
+    func testSixCharacterCount() {
+        let color = UIColor(hex: "#FFFFFF")
+        XCTAssertNotNil(color, "Should not be nil.")
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testOtherCharacterCount() {
+        let color = UIColor(hex: "#F")
+        XCTAssertNil(color, "Should be nil.")
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAlphaChannel() {
+        let color = UIColor(hex: "#FFF", alpha: 0.5)
+        XCTAssertNotNil(color, "Should not be nil.")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
